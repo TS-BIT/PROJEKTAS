9023867284875377dc35ed52ace3e3497d22efd0
TS BIT <tomassag@gmail.com>
2021-08-04 08:41:04 +0300

    Final changes 0803

---
+++ src/index0804dser.js
@@ -0,0 +1,60 @@
+import { Server } from "net";
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const WEB_DIR = "web";
+
+let server = new Server((socket) => {
+  socket.setEncoding("utf-8");
+  let allData = "";
+  socket.on("data", async (data) => {
+    allData += data;
+    let lines = allData.split("\r\n");
+    if (lines.findIndex((e) => e === "") !== -1) {
+      let fileName = lines[0].split(" ")[1];
+      let resp;
+      if (fileName === "/end") {
+        resp = "HTTP/1.1 200 Ok\r\n\r\n";
+        resp += "<html><body><b>Server is going down. Bye bye.</b></body></html>";
+      } else {
+        let realFile = path.join(WEB_DIR, fileName);
+        try {
+          resp = "HTTP/1.1 200 Ok\r\n\r\n";
+          const stats = await fs.stat(realFile);
+          let html;
+          if (stats.isDirectory()) {
+            if (!fileName.endsWith("/")) {
+              fileName += "/";
+            }
+            const filesInDir = await fs.readdir(realFile);
+            html = "<html><body>";
+            if (fileName !== "/") {
+              html += '<a href="' + fileName + '../">..</a><br>';
+            }
+            for (const f of filesInDir) {
+              html += '<a href="' + fileName + f + '">' + f + "</a><br>";
+            }
+            html += "</body></html>";
+          } else {
+            html = await fs.readFile(realFile, {
+              encoding: "utf-8",
+            });
+          }
+          resp += html;
+        } catch (err) {
+          resp = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+      }
+      socket.write(resp + "\r\n\r\n", () => {
+        socket.end();
+        if (fileName === "/end") {
+          socket.destroy();
+          server.close();
+        }
+      });
+    }
+  });
+});
+
+server.listen(3000);
+console.log("Server started");
--- src/indexserv.js
+++ src/indexserv.js
@@ -5,13 +5,6 @@ import * as path from "path";
 const WEB_DIR = "web";
 
 let server = new Server((socket) => {
-    // console.log("kazkas prisijunge");
-    // console.log(
-    //     socket.localAddress,
-    //     socket.localPort,
-    //     socket.remoteAddress,
-    //     socket.remotePort,
-    // );
     socket.setEncoding("utf-8");
     let allData = "";
     socket.on("data", async (data) => {
@@ -19,56 +12,53 @@ let server = new Server((socket) => {
         let lines = allData.split("\r\n");
         if (lines.findIndex(e => e === "") !== -1) {
             let fileName = lines[0].split(" ")[1];
-            let realFile = path.join(WEB_DIR, fileName);
-            console.log(realFile);
             let resp;
-            if (fileName === "/end") {
-                resp = "HTTP/1.1. 200 Ok\r\n\r\n";
-            
-            
-                try {
+            if(fileName === "/end") {
+                resp = "HTTP/1.1 200 Ok\r\n\r\n";
+                resp += "<html><body><b>Server is going down. Bye bye.</b></body></html>";
+            } else {
+            let realFile = path.join(WEB_DIR, fileName);
+            try {
                 resp = "HTTP/1.1 200 Ok\r\n\r\n";
                 const stats = await fs.stat(realFile);
                 let html;
                 if (stats.isDirectory()) {
                     if (!fileName.endsWith("/")) {
-
-                    const filesInDir =  await fs.readdir(realFile);
+                        fileName += "/";
+                    }
+                    const filesInDir = await fs.readdir(realFile);
                     html = "<html><body>";
-
+                    if (fileName !== "/") {
+                        html += '<a href="' + fileName + '../">..</a><br>';
+                    }
+                    for (const f of filesInDir) {
+                        html += '<a href="' + fileName + f + '">' + f + "</a><br>";
+                    }
+                    html += "</body></html>"; 
+                } else {
+                    html = await fs.readFile(realFile, {
+                        encoding: "utf-8"
+                    }); 
                 }
-                    encoding: "utf-8"
-                });
                 resp += html;
             } catch (err) {
                 resp = "HTTP/1.1 404 Not Found\r\n\r\n";
             }
-            console.log(resp);
+        }  
             socket.write(resp + "\r\n\r\n", () => {
                 socket.end();
-            });
-            if (fileName === "/end") {
-                socket.destroy();
-                server.close();
-            };
-
-            //console.log(fileName, realFile); 
-            //let lines = allData.split("\r\n");
-            // console.log(allData);
-            // console.log("-----------");
-            // let resp = "HTTP/1.1 200 Ok\r\n\r\n";
-            // let html = "<html><body><h1>Sveiki Visi !!!</h1></body></html>";
-            // console.log(resp + html + "\r\n\r\n");
-            // console.log("=============");
-            // socket.write(resp + html + "\r\n\r\n", () => {
-            //     socket.end();
-            // });
+                if (fileName === "/end") {
+                    socket.destroy();
+                    server.close();
+                }
+            });  
         }
     });
 });
 
 
 server.listen(3000);
+console.log("Server started");
 
 
 
